c02479a0ae5c9058b9bf3d2f4a83a2dda6d4535d
Cedric Smith <cedricsmith@Cedrics-iMac.local>
2021-04-11 17:43:22 -0700

    first commit

---
+++ week5.js
@@ -0,0 +1,12 @@
+let p = $('#test');
+let div = $('.my-class');
+let ul = $('ul');
+
+console.log(p);
+console.log(div);
+console.log(ul);
+
+console.log(p.text());
+p.text('New Text');
+
+$('input').val('New Value');
